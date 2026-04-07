acdb0c0bf935bd745e72284171af6b603177c17f
vikashb67 <vikasworks67@gmail.com>
2026-04-07 09:29:00 +0530

    Added DFS algm

---
+++ DFS_algorithm.py
@@ -0,0 +1,14 @@
+def dfs_iterative(tree, start):
+    visited = set()
+    stack = [start]
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            # Add neighbors in reverse order to maintain left-to-right traversal
+            stack.extend(reversed(tree[node]))
+
+print("\nDFS Traversal (Iterative):")
+dfs_iterative(tree, 1)
